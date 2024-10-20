fe6e77aed3ea29ff0010058a131e2aed8958d75c
Petr S <petrsmilek@seznam.cz>
2024-10-20 16:11:51 +0200

    functionality ok

--- controllers/PlayerController.php
+++ controllers/PlayerController.php
@@ -18,19 +18,38 @@ class PlayerController {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $name = $_POST['name'];
             $this->playerModel->createPlayer($name);
-            header('Location: /');
+            header('Location: index.php');
+            exit;
         }
 
-        $player = $this->playerModel->getPlayer($_GET['id']);
-        include('views/edit_player.php');
+        include('views/create_player.php');
     }
+
+    public function editPlayer() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $id = $_POST['id'];
+            $name = $_POST['name'];
+            $rating = $_POST['rating'];
+    
+            $this->playerModel->updatePlayer($id, $name, $rating);
+    
+            header('Location: index.php');
+            exit;
+        }
+    
+        if (isset($_GET['id'])) {
+            $player = $this->playerModel->getPlayer($_GET['id']);
+            include('views/edit_player.php');
+        } else {
+            echo "Hráč nebyl nalezen.";
+        }
+    }    
 
     public function deletePlayer() {
         $id = $_GET['id'];
         $this->playerModel->deletePlayer($id);
-        header('Lodation: /');
+        header('Location: index.php');
     }
-
 }
 
 ?>
--- views/players_list.php
+++ views/players_list.php
@@ -1,11 +1,11 @@
 <h1>Seznam hráčů</h1>
-<a href="/?action=create">Přidat nového hráče</a>
+<a href="./index.php?action=create">Přidat nového hráče</a>
 <ul>
     <?php foreach ($players as $player): ?>
     <li>
         <?= htmlspecialchars($player['name']) ?> (Elo: <?= $player['elo_rating'] ?>)
-        <a href="/?action=edit&id=<?= $player['id'] ?>">Editovat</a>
-        <a href="/?action=delete&id=<?= $player['id'] ?>" onclick="return confirm('Opravdu chcete smazat?');">Smazat</a>
+        <a href="./index.php?action=edit&id=<?= $player['id'] ?>">Editovat</a>
+        <a href="./index.php?action=delete&id=<?= $player['id'] ?>" onclick="return confirm('Opravdu chcete smazat?');">Smazat</a>
     </li>
     <?php endforeach; ?>
 </ul>
