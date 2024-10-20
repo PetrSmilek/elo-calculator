29a3bc8006dd680a428527f1200d437b8578a765
Petr S <petrsmilek@seznam.cz>
2024-10-20 18:52:36 +0200

    composer config

--- config/database.php
+++ config/database.php
@@ -1,8 +1,17 @@
 <?php
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
 function getDbConnection() {
     try {
-        $db = new PDO('mysql:host=localhost;dbname=elo_db', 'root', 'Alechin9');
+        $db = new PDO(
+            'mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_NAME'], 
+            $_ENV['DB_USER'], 
+            $_ENV['DB_PASS']
+        );
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $db;
     } catch (PDOException $e) {
